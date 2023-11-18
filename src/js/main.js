7130180b7acb98156ab63aca74974f0fa7833558
Zakroev Andrey <Dr.zakroev@yandex.ru>
2023-11-18 21:06:56 +0300

    add mini sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,35 @@
-import { MainSlider, VideoPlayer } from "../js/modules";
+import { MainSlider, VideoPlayer, MiniSlider } from "../js/modules";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const slider = new MainSlider({ page: ".page", buttons: ".next" });
+  const slider = new MainSlider({ container: ".page", buttons: ".next" });
   slider.render();
+
+  const showUpSlider = new MiniSlider({
+    container: ".showup__content-slider",
+    prev: ".showup__prev",
+    next: ".showup__next",
+    activeClass: "card-active",
+    animate: true,
+  });
+  showUpSlider.init();
+
+  const modulesSlider = new MiniSlider({
+    container: ".modules__content-slider",
+    prev: ".modules__info-btns .slick-prev",
+    next: ".modules__info-btns .slick-next",
+    activeClass: ".card-active",
+    animate: true,
+    autoplay: true,
+  });
+  modulesSlider.init();
+
+  const feedSlider = new MiniSlider({
+    container: ".feed__slider",
+    prev: ".feed__slider .slick-prev",
+    next: ".feed__slider .slick-next",
+    activeClass: "feed__item-active",
+  });
+  feedSlider.init();
 
   const player = new VideoPlayer(".showup .play", ".overlay");
   player.init();
--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,3 +1,4 @@
 export { default as VideoPlayer } from "./playVideo";
 export { default as Slider } from "./slider/slider";
 export { default as MainSlider } from "./slider/slider-main";
+export { default as MiniSlider } from "./slider/slider-mini";
